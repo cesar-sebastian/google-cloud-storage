292db264e14feabd217c1e15f70063eda4bb9b33
CSC <cesarcarrazana@gmail.com>
2020-05-30 16:50:25 -0300

    integrate gtoken

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
-
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.port || 3000;
 
@@ -9,9 +9,9 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const google = require('./google.js');
-// first we need to generate the JWT signature, then we need to get an access token from google, and send it back
-app.get('/token', google.createSignature, google.getAccessToken, (req, res) => {
-  res.json({token: res.access_token});
+
+app.get('/token', google.getAccessToken, function(req, res){	
+	res.json({token: res.access_token});  
 });
 
 app.listen(port, () => console.warn(`Listening on port ${port}!`));
